546301b572d3a5085d59012f7b328cbc1023fbde
Marijn Bent <marijn@marijnbent.nl>
2025-02-27 23:59:21 +0300

    Remove old code

--- src/resources/value-converters/round.js
+++
@@ -1,9 +0,0 @@
-export class FloatformatValueConverter {
-  toView(value, precision = 2) {
-    if (isNan(value) || value < 1) {
-      return value;
-    }
-
-    return value.toPrecision(precision);
-  }
-}
